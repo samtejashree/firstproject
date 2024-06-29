78ef09ada453135c786137f8988480553a267815
samtejashree <stejaswini53@gmail.com>
2024-06-29 16:20:00 +0530

    create multiple components

--- App.js
+++ App.js
@@ -4,11 +4,34 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <CompanyData />
+      <Text style={{ fontSize: 30 }}>Components</Text>
+      <UserData />
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const UserData = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 20 }}>Name : Teju</Text>
+      <Text style={{ fontSize: 20 }}>Age : 24</Text>
+      <Text style={{ fontSize: 20 }}>Email : Teju@gmail.com</Text>
+    </View>
+  )
+}
+
+const CompanyData = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 20 }}>CompanyName : Dambaruu</Text>
+      <Text style={{ fontSize: 20 }}>Total Emp : 100</Text>
+      <Text style={{ fontSize: 20 }}>Product : ABC Product</Text>
+    </View>
+  )
+}
+
 
 const styles = StyleSheet.create({
   container: {
